b820598c0af3f5f9752765d0f9917e15e7aa0084
Nolan Byrnes <nolanehs@gmail.com>
2017-12-14 15:25:31 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -37,11 +37,11 @@ scraped_student = {}
 doc.css(".social-icon-container a").each do |link|
   if link["href"].include?("twitter")
          scraped_student[:twitter] = link["href"]
-       elsif link["href"].include?("github")
-         scraped_student[:github] = link["href"]
        elsif link["href"].include?("linkedin")
          scraped_student[:linkedin] = link["href"]
-       elsif scraped_student[:blog] = link["href"]
+       elsif link["href"].include?("github")
+         scraped_student[:github] = link["href"]
+              elsif scraped_student[:blog] = link["href"]
        end
 
   end
