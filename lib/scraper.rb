90215b22ec059fbeb2fd7c561a57c32fe795d799
Nolan Byrnes <nolanehs@gmail.com>
2017-12-14 13:53:57 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,36 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
+
 
 class Scraper
 
+
+
+
   def self.scrape_index_page(index_url)
-    
-  end
+# for each student, parse the name (class 'student-name', location (class 'student-location') profile_url (a href)
+html = open("./fixtures/student-site/index.html")
+doc = Nokogiri::HTML(html)
+
+student_name = doc.css(".student-card")
+scraped_students = []
+ student_name.each do |index|
+ scraped_students <<{
+  :name => index.css(".student-name").text,
+  :location => index.css(".student-location").text,
+  :profile_url => index.css("a").attribute("href").value
+
+
+}
+
+end
+scraped_students
+end
+
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
